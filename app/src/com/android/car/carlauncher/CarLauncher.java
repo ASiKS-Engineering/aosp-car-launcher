1b73d653125e5f5e6e73f7baa38c0c57a5c67858
ASiKS-Engineering <info@asiks-engineering.com>
2026-09-25 16:29:51 +0200

    Revert Z order

--- app/src/com/android/car/carlauncher/CarLauncher.java
+++ app/src/com/android/car/carlauncher/CarLauncher.java
@@ -296,9 +296,9 @@ public class CarLauncher extends FragmentActivity {
                 }
                 container.addView(taskView, 0);
 
-                // FIX 4: Z-Order anpassen für korrektes Surface-Compositing von MapLibre
-                taskView.setZOrderOnTop(true);
-                taskView.setZOrderMediaOverlay(false);
+                //Die Karte hinter den Launcher schieben, aber sichtbar halten
+                taskView.setZOrderOnTop(false);
+                taskView.setZOrderMediaOverlay(true);
                 taskView.setObscuredTouchRegion(null);
 
                 if (mMapsPlaceholder != null) mMapsPlaceholder.setVisibility(View.GONE);
